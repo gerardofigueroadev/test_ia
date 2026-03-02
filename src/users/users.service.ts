ceba0435864419027d519f117c78c52d052c0d28
jcuellart <jcuellart@bancosol.com.bo>
2026-03-02 09:53:01 -0400

    test: add weird user update method for code smell detection

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -54,4 +54,52 @@ export class UsersService {
     this.users = this.users.filter((u) => u.id !== id);
     return user;
   }
+
+  wirdUpdate(id: string, data: any): any {
+    let result: any = null;
+    let found = false;
+
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i].id == id) {
+        found = true;
+
+        if (data != null) {
+          if (data.username != undefined && data.username != null) {
+            for (let j = 0; j < this.users.length; j++) {
+              if (this.users[j].username == data.username && this.users[j].id != id) {
+                throw new ConflictException("username already exists");
+              }
+            }
+            this.users[i].username = data.username;
+          } else {
+            console.log("username is null or undefined");
+          }
+
+          if (data.password) {
+            if (data.password.length < 3) {
+              console.log("password too short");
+            } else {
+              this.users[i].password = data.password;
+            }
+          } else {
+            console.log("no password provided");
+          }
+
+          if (data.randomFlag == true) {
+            this.users[i]["randomField"] = "something";
+          }
+
+          result = this.users[i];
+        } else {
+          console.log("data is null");
+        }
+      }
+    }
+
+    if (!found) {
+      throw new NotFoundException("user not found");
+    }
+
+    return result;
+  }
 }
